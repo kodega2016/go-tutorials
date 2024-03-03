6ae6953be6a6e303164aa71df564d0c07bee8ecc
kodega2016 <khadgalovecoding2016@gmail.com>
2024-03-03 20:45:55 +0545

    added range for maps

--- range/main.go
+++ range/main.go
@@ -11,4 +11,16 @@ func main() {
 
 	fmt.Println("total scores:", totalScores)
 	fmt.Println("average score:", totalScores/len(scores))
+
+	var userDoc map[string]any = map[string]any{
+		"name":          "khadga bahadur shrestha",
+		"score":         12,
+		"is_active":     true,
+		"leave_balance": 2.4,
+	}
+
+	fmt.Println(userDoc)
+	for key, value := range userDoc {
+		fmt.Println(key, value)
+	}
 }
